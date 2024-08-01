147358a34d4a926d1b312ec50353fbe6028f285c
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-01 23:53:47 +0400

    Adding some views and their urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Works</h1>")
+
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Voting on question {question_id}")
