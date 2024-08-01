961dd7c0d7211d23430e9be883a89d0c6d7e0994
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-01 23:06:24 +0400

    Adding representation

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,9 +4,20 @@ class Question(models.Model):
     question_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text   
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
+
 # python3 manage.py makemigrations polls
+# python3 manage.py shell
+# >>> from polls.models import Choice, Question
+# >>> from django.utils import timezone
+# >>> q = Question(question_text="What's new", pub_date = timezone.now())
+# >>> q.save()
