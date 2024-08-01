20c2b2558b8446086a8e2f458043d08f6fa1d9bf
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-01 23:22:00 +0400

    Comments about working in shell

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,9 +15,29 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-# python3 manage.py makemigrations polls
-# python3 manage.py shell
-# >>> from polls.models import Choice, Question
-# >>> from django.utils import timezone
-# >>> q = Question(question_text="What's new", pub_date = timezone.now())
-# >>> q.save()
+# Some lookups
+
+# Question.objects.filter(id=1)
+# Question.objects.filter(question_text__startswith="What")
+
+
+# Get question that published this year
+# from django.utils import timezone
+# current_year = timezone.now().year
+# Question.objects.get(pub_date__year=current_year)
+
+# Lookup by primary key
+# Question.objects.get(pub_date__year=current_year)
+
+
+# --Giving Question couple of Choices
+# q = Question.objects.get(pk=1)
+# q.choice_set.all() --> <QuerySet []>
+# q.choice_set.create(choice_text="Not much", votes=0)
+# q.choice_set.create(choice_text="The sky", votes=0)
+# c = q.choice_set.create(choice_text="Just hacking again", votes=0)
+# c.question
+# q.choice_set.all()
+# Choice.objects.filter(question__pub_date__year=current_year)
+# c = q.choice_set.filter(choice_text__startswith="Just")
+# c.delete()
