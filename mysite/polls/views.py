f9f10a2cc09bbd4844eacd668a91e1dccc712124
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-02 14:06:31 +0400

    Configuring voting view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.template import loader
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -19,4 +20,19 @@ def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
