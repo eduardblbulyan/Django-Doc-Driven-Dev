4ffcd81db4aba1d172d02b024cd43d7e5dfdab3c
Eduard Blbulyan <edwardblbulyan@gmail.com>
2024-08-02 19:36:26 +0400

    Adding more complex tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,4 +9,16 @@ class QuestionModelTest(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_questinon = Question(pub_date=time)
+        self.assertIs(recent_questinon.was_published_recently(), True)
+
+
+
 # $ python3 manage.py test polls
